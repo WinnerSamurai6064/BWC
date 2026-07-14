460dbf20f1ac0ff100efaf2ec91c81a53ce72280
WinnerSamurai6064 <ikegbuneagwu@gmail.com>
2026-07-14 16:19:45 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+export default defineConfig({
+  plugins: [svelte()],
+
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    strictPort: true
+  },
+
+  build: {
+    target: 'esnext',
+    sourcemap: false,
+    minify: 'esbuild'
+  }
+});
